91c50ad68191a4ccf2fe4c928006a946adeecb46
amit0617 <71893015+Amit0617@users.noreply.github.com>
2023-03-14 15:35:57 +0530

    update

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,7 @@
 import pysimilar
 pysimilar.extensions= '.txt'
-comparison_result = pysimilar.compare_documents('uploaded_files')
+try:
+    comparison_result = pysimilar.compare_documents('uploaded_files')
+except pysimilar.SimilarError as e:
+    print(e)
 print(comparison_result)
