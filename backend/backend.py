7b1f59425448d6f42c6f28346b826d68e0d1a94d
amit0617 <71893015+Amit0617@users.noreply.github.com>
2023-03-14 15:53:19 +0530

    update backend

--- backend/backend.py
+++ backend/backend.py
@@ -1,9 +1,19 @@
 import pysimilar
 import keyboard
-
 pysimilar.extensions= '.txt'
-try:
-    comparison_result = pysimilar.compare_documents('uploaded_files')
-except pysimilar.SimilarError as e:
-    print(e)
-print(comparison_result)
+while True:
+    try:
+        if keyboard.is_pressed('q'):
+            break
+        else:
+            pass
+            comparison_result = pysimilar.compare_documents('uploaded_files')
+            # print(e)
+            print(comparison_result)
+    except:
+        break
+
+
+
+# try:
+# except pysimilar.SimilarError as e:
