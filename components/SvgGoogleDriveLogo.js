1fb1df986c596aeb8daacf7a7b7eb399ec09f389
amit0617 <71893015+Amit0617@users.noreply.github.com>
2023-04-22 16:15:57 +0530

    Add google log

---
+++ components/SvgGoogleDriveLogo.js
@@ -0,0 +1,152 @@
+import * as React from "react";
+const SvgGoogleDriveLogo = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    // viewBox="0 0 287.5 250"
+    height={props.height || "100%"}
+    width={props.width || "100%"}
+    viewBox="0 0 287.5 260"
+    // xmlSpace="preserve"
+    role="img"
+    {...props}
+  >
+    <defs>
+      <path
+        id="Google_Drive_logo_%282014-2020%29_svg__a"
+        d="M190.7 0H96.8L0 168.8 45.8 250h195.9l45.8-81.2L190.7 0zM92.9 168.8 143.8 81l50.9 87.8H92.9z"
+      />
+    </defs>
+    <clipPath id="Google_Drive_logo_%282014-2020%29_svg__b">
+      <use
+        xlinkHref="#Google_Drive_logo_%282014-2020%29_svg__a"
+        overflow="visible"
+      />
+    </clipPath>
+    <g clipPath="url(#Google_Drive_logo_%282014-2020%29_svg__b)">
+      <path fill="#4285F4" d="M45.8 168.8V250h195.9l45.8-81.2H45.8z" />
+      <linearGradient
+        id="Google_Drive_logo_%282014-2020%29_svg__c"
+        gradientUnits="userSpaceOnUse"
+        x1={6263.692}
+        y1={-4322.291}
+        x2={6831.43}
+        y2={-4630.316}
+        gradientTransform="matrix(.125 0 0 -.125 -713.344 -330.75)"
+      >
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#1a237e",
+            stopOpacity: 0.2,
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#1a237e",
+            stopOpacity: 0.02,
+          }}
+        />
+      </linearGradient>
+      <path
+        fill="url(#Google_Drive_logo_%282014-2020%29_svg__c)"
+        d="M92.9 168.8 140 250H45.8l47.1-81.2z"
+      />
+      <path
+        fill="#FFF"
+        fillOpacity={0.2}
+        d="M92 170.3h192.9l2.7-1.6H92.9l-.9 1.6z"
+      />
+      <path
+        fill="#1A237E"
+        fillOpacity={0.2}
+        d="m284.8 170.3-44 78.1H46.7l-.9 1.6h195.9l45.8-81.2-2.7 1.5z"
+      />
+      <path
+        fill="#1A237E"
+        fillOpacity={0.05}
+        d="m93.8 170.3-.9-1.6L45.8 250h1.8l46.2-79.7z"
+      />
+    </g>
+    <defs>
+      <path
+        id="Google_Drive_logo_%282014-2020%29_svg__d"
+        d="M190.7 0H96.8L0 168.8 45.8 250h195.9l45.8-81.2L190.7 0zM92.9 168.8 143.8 81l50.9 87.8H92.9z"
+      />
+    </defs>
+    <clipPath id="Google_Drive_logo_%282014-2020%29_svg__e">
+      <use
+        xlinkHref="#Google_Drive_logo_%282014-2020%29_svg__d"
+        overflow="visible"
+      />
+    </clipPath>
+    <g clipPath="url(#Google_Drive_logo_%282014-2020%29_svg__e)">
+      <path fill="#0F9D58" d="M96.8 0 0 168.8 45.8 250 156.2 59.4 96.8 0z" />
+      <path
+        fill="#263238"
+        fillOpacity={0.1}
+        d="m96.8 0 .9 4.6 44.2 76.3-96.1 165.9-44-78.1H0L45.8 250l98-169.1L96.8 0z"
+      />
+      <path
+        fill="#FFF"
+        fillOpacity={0.2}
+        d="m1.8 168.8 96-167.2-1-1.6L0 168.8h1.8z"
+      />
+    </g>
+    <defs>
+      <path
+        id="Google_Drive_logo_%282014-2020%29_svg__f"
+        d="M190.7 0H96.8L0 168.8 45.8 250h195.9l45.8-81.2L190.7 0zM92.9 168.8 143.8 81l50.9 87.8H92.9z"
+      />
+    </defs>
+    <clipPath id="Google_Drive_logo_%282014-2020%29_svg__g">
+      <use
+        xlinkHref="#Google_Drive_logo_%282014-2020%29_svg__f"
+        overflow="visible"
+      />
+    </clipPath>
+    <g clipPath="url(#Google_Drive_logo_%282014-2020%29_svg__g)">
+      <path fill="#FFCD40" d="M190.7 0H96.8l97.8 168.8h92.9L190.7 0z" />
+      <path
+        fill="#FFF"
+        fillOpacity={0.2}
+        d="M190.7 0H96.8l2.7 1.6h90.2l96 167.2h1.8L190.7 0z"
+      />
+      <path
+        fill="#BF360C"
+        fillOpacity={0.1}
+        d="M195.5 167.2 99.5 1.6 96.8 0l97.8 168.8.9-1.6z"
+      />
+    </g>
+    <radialGradient
+      id="Google_Drive_logo_%282014-2020%29_svg__h"
+      cx={6486.35}
+      cy={-2666.212}
+      r={2290.675}
+      gradientTransform="matrix(.125 0 0 -.125 -713.344 -330.75)"
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop
+        offset={0}
+        style={{
+          stopColor: "#fff",
+          stopOpacity: 0.1,
+        }}
+      />
+      <stop
+        offset={1}
+        style={{
+          stopColor: "#fff",
+          stopOpacity: 0,
+        }}
+      />
+    </radialGradient>
+    <path
+      fill="url(#Google_Drive_logo_%282014-2020%29_svg__h)"
+      d="M190.7 0H96.8L0 168.8 45.8 250h195.9l45.8-81.2L190.7 0zM92.9 168.8 143.8 81l50.9 87.8H92.9z"
+    />
+  </svg>
+);
+export default SvgGoogleDriveLogo;
+
